d4faf66eb1b5dda84f2bf55b9b6f607d16e4b774
William Hambrick <detan.shi@gmail.com>
2019-10-09 08:28:31 -0500

    Updating of Bootstrap

--- Destiny/clan-search.php
+++ Destiny/clan-search.php
@@ -73,6 +73,7 @@
     <!-- Search form -->
     <div class="container">
       <form class="example">
+        <h1>Search for a Clan</h1>
         <input type="text" placeholder="Search.." name="search">
         <button type="submit"><i class="fa fa-search"></i></button>
       </form>
@@ -85,17 +86,120 @@
     <?php
 
       if(isset($_GET['search'])){
-        echo '<h1> '.$_GET['search'].' </h1>';
+
+        $clan_name = $_GET['search'];
+
+        //$clan_name = str_replace(' ',"%20",$clan_name);
+        //$clan_name = str_replace("'","&#39;",$clan_name);
+
+        $clan_name = urlencode ( $clan_name ); 
+
+        $clan_name = str_replace('+',"%20",$clan_name);
+
         echo '<br>';
         
-        $api_call = curl_init();
-        curl_setopt($api_call, CURLOPT_URL, 'https://www.bungie.net/Platform/Destiny2/GroupV2/Name/'.$_GET['search'].'/1/');
-        curl_setopt($api_call, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($api_call, CURLOPT_HTTPHEADER, array('X-API-Key: ' . $apiKey));
-
-        $json = json_decode(curl_exec($api_call));
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.bungie.net/Platform/GroupV2/Name/'.$clan_name.'/1/');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-API-Key: ' . $apiKey));
+       
+        $json = json_decode(curl_exec($ch));
         
-        print_r($json);
+        //echo "<br><br>Debug<br>";
+        //print_r(curl_getinfo($ch));
+
+        if(isset($json)){
+
+          echo '<h1>'.$json->Response->detail->name.'</h1>';
+          echo '<p><i>'.$json->Response->detail->motto.'</i></p>';
+          echo '<p>'.$json->Response->detail->about.'</p>';
+          echo '<p>Member Count: '.$json->Response->detail->memberCount.'/'.$json->Response->detail->features->maximumMembers.'</p>';
+
+          $member_call = curl_init();
+          curl_setopt($member_call, CURLOPT_URL, 'https://www.bungie.net/Platform/GroupV2/'.$json->Response->detail->groupId.'/Members/');
+          curl_setopt($member_call, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($member_call, CURLOPT_HTTPHEADER, array('X-API-Key: ' . $apiKey));
+         
+          $member_json = json_decode(curl_exec($member_call));
+
+          echo '<br><h2>Public Member List</h2><br><br>';
+
+          echo '<table>';
+
+          $results = $member_json->Response->results;
+
+          $i = 0; 
+
+          while($i < count($results)){
+
+            echo '<tr>';
+            echo '<td style="border-collapse: collapse; border:1pt solid black;">';
+
+            if($results[$i]->destinyUserInfo->crossSaveOverride != 0){
+              echo '<img src="https://i.imgur.com/4f3xKlT.png" style="margin-left: 0.5%; width: 6.5%; height: 8.5%;">';
+            } else {
+
+              switch($results[$i]->destinyUserInfo->membershipType){
+
+                case 1:
+                    echo '<img src="https://i.imgur.com/BevwBOg.png" style="margin-left: 1.5%; width: 6.5%; height: 6.5%;">';
+                    break;
+                case 2:
+                    echo '<img src="https://i.imgur.com/0WIQXLq.png"  style="width: 10%; height: 18%;">';
+                    break;
+                case 3:
+                    echo '<img src="https://i.imgur.com/PVWMEwR.png"  style="width: 10%; height: 18%;">';
+                    break;
+                case 4:
+                    echo '<img src="https://i.imgur.com/UfnuPfw.png"  style="width: 10%; height: 21%;">';
+                    break;
+                case 5:
+                    echo '<img src="https://i.imgur.com/1XIuoKN.png"  style="width: 15%; height: 15%;">';
+                    break;
+              }
+            }
+
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$results[$i]->destinyUserInfo->displayName;
+            
+            if($results[$i]->isOnline == 1){
+              echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Status: Online]&nbsp;&nbsp;&nbsp;</td>';
+            } else {
+              echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Status: <i>Offline</i>]&nbsp;&nbsp;&nbsp;</td>';
+            }
+
+            switch($results[$i]->memberType){
+
+              case 1:
+                  echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Beginner]&nbsp;&nbsp;&nbsp;</td>';
+                  break;
+              case 2:
+                  echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Member]&nbsp;&nbsp;&nbsp;</td>';
+                  break;
+              case 3:
+                  echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Admin]&nbsp;&nbsp;&nbsp;</td>';
+                  break;
+              case 4:
+                  echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Acting Founder]&nbsp;&nbsp;&nbsp;</td>';
+                  break;
+              case 5:
+                  echo '<td style="border-collapse: collapse; border:1pt solid black;">&nbsp;&nbsp;&nbsp;[Founder]&nbsp;&nbsp;&nbsp;</td>';
+                  break;
+            }
+
+            echo '</td>';
+            echo '</tr>';
+            $i++;
+          }
+
+          echo '</table>';
+
+          echo '<br><br>';
+          //print_r($member_json);
+        } else {
+          echo 'An error occured in lookup, please try again. It is possible the API could not locate anything to return and simply did not return any information';
+        }
+
+        
         
       }
 
@@ -109,7 +213,7 @@
 
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">William Hambrick 2019 ©</span>
+    <span class="text-muted">© William Hambrick 2019</span>
   </div>
 </footer>
 <script src="./Sticky Footer Navbar Template · Bootstrap_files/jquery-3.3.1.slim.min.js.download" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
